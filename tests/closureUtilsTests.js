2c89843dc5325c70ae91ddf57dd91acb3be0a15d
Guido Tapia <guido@tapia.com.au>
2011-02-03 02:39:22 +0100

    Added some tests to utils.js

--- tests/closureUtilsTests.js
+++ tests/closureUtilsTests.js
@@ -1,17 +1,31 @@
-#!node
+#!/usr/local/bin/node
 
 var ng_ = require('goog').goog.init();
-var utils = ng_.getUtils();
+var utils_ = ng_.getUtils();
 
 goog.require('goog.testing.jsunit');
 
-testReadSettingObjectFromFile = function() {  
+testReadSettingObjectFromFile = function() {
 };
 
-testReadSettingObjectFromDir = function() {  
+testReadSettingObjectFromDir = function() {
 };
 
-testGetPath = function() {};
+testGetPath = function() {
+  assertEquals('/dir/file.ext', utils_.getPath('/dir', 'file.ext'));
+  assertEquals('/dir/file.ext', utils_.getPath('/dir/', 'file.ext'));
+  assertEquals('/dir/file.ext', utils_.getPath('/dir/', '/file.ext'));
+  assertEquals('dir1/dir2/file.ext', utils_.getPath('dir1/dir2', '/file.ext'));
+};
 
-testExtendObject() {};
+testExtendObject = function() {
+  var o1 = {prop1:1}
+  assertEquals(2, utils_.extendObject_(o1, {prop1:2}).prop1);
+  utils_.extendObject_(o1, {prop2:3});
+  assertEquals(2, o1.prop1);
+  assertEquals(3, o1.prop2);
+  utils_.extendObject_(o1, {});
+  assertEquals(2, o1.prop1);
+  assertEquals(3, o1.prop2);
+};
 
